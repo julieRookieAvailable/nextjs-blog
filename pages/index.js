893f62ba0afdbd6e5e9c22d22833043a3d0d4bda
LAPTOP-53K14TQG\Juliette Sanchez <juliettesv50@gmail.com>
2023-10-29 17:43:03 -0500

    cambié mi nombre

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
        
-       <p>Hola, soy Juliette y este es mi blog personal para practicar en Next.Js</p>
+       <p>Hola, soy Juliette Aimée y este es mi blog personal para practicar en Next.Js</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
